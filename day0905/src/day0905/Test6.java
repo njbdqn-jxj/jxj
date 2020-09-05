f5f2f40df28b2501c99e72a53f27c0890d57c150
xj <xj@jxj>
2020-09-05 12:03:17 +0800

    add two

---
+++ day0905/src/day0905/Test6.java
@@ -0,0 +1,12 @@
+package day0905;
+
+public class Test6
+{
+    
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("asd");
+    }
+    
+}
